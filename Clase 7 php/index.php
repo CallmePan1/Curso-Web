af568bc4d5c5dd8b692e03220907eb3c9abef31d
CallmePan1 <franciscorotundo@gmail.com>
2022-11-25 21:47:34 -0300

    php?

---
+++ Clase 7 php/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Punto 7</title>
+</head>
+<body>
+<?php
+print "Esto es la prueba 1";
+?>
+<?php
+print "<h1> Esto es un texto feo</h1>";
+
+?>
+<?php print "<h2>Diosmio al fin sale mi texto</h2>"; ?>
+</body>
+</html>
